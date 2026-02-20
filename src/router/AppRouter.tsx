dc5c7b130d075d7020b1898bcc228116435921a4
Fenrir <Alejandro Morales>
2026-02-20 19:33:09 -0300

    EVO 02: Router configurado con rutas listas

---
+++ src/router/AppRouter.tsx
@@ -0,0 +1,23 @@
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import {Layout} from "../components/layout/Layout.tsx";
+import {TodoListPage} from '../pages/TodoListPage.tsx';
+import {TodoCreatePage} from '../pages/TodoCreatePage.tsx';
+import {TodoDetailPage} from '../pages/TodoDetailPage.tsx';
+import {NotFoundPage} from '../pages/NotFoundPage.tsx';
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Layout />,
+        children: [
+            {index: true,element: <TodoListPage/>},
+            {path: "new", element: <TodoCreatePage/>},
+            {path: "todo/:id", element: <TodoDetailPage/>},
+            {path: "*", element: <NotFoundPage/>}
+
+        ]
+    }
+])
+export function AppRouter() {
+    return <RouterProvider router={router}/>;
+}
