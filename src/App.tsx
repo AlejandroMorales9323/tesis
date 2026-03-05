ddbcf8b6cfecf848cf59104e08a23c22f3ab4eef
Fenrir <Alejandro Morales>
2026-03-05 18:15:53 -0300

    feat: evo-8 context de preferencias

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import {AppRouter} from './router/AppRouter.tsx';
 import { PreferencesProvider } from './context/PreferencesContext';
+// se importa en la app la funcion que hicimos dentro de preferencescontext
+// y se envuelve el approuter con la funcion preference provider
 function App() {
     return(
         <PreferencesProvider>
